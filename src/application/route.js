5bc2068bea0950ba8ceee929ac12d2eb3d6237f9
Rizki Nuzul Muzaki <rizki@qiscus.net>
2024-05-25 23:20:00 +0700

    fix: add prefix api/v1 in route

--- src/application/route.js
+++ src/application/route.js
@@ -2,7 +2,7 @@ import express from "express";
 import webhookController from "../webhooks/controller.js";
 
 const router = new express.Router();
-router.post('/webhooks/caa', webhookController.assign);
-router.post('/webhooks/resolve', webhookController.resolve);
+router.post('/api/v1/webhooks/caa', webhookController.assign);
+router.post('/api/v1/webhooks/resolve', webhookController.resolve);
 
 export default router;
