4917cb51c992c439d1481e0384d92ac6c55db5ac
Rizki Nuzul Muzaki <rizki@qiscus.net>
2024-05-26 08:04:04 +0700

    fix: improve route naming

--- src/application/route.js
+++ src/application/route.js
@@ -3,7 +3,7 @@ import webhookController from "../webhooks/controller.js";
 import syncAgent from "../middlewares/syncAgent.js";
 
 const router = new express.Router();
-router.post('/api/v1/webhooks/caa', syncAgent, webhookController.assign);
-router.post('/api/v1/webhooks/resolve', syncAgent, webhookController.resolve);
+router.post('/api/v1/webhooks/allocation', syncAgent, webhookController.assign);
+router.post('/api/v1/webhooks/mark-as-resolved', syncAgent, webhookController.resolve);
 
 export default router;
