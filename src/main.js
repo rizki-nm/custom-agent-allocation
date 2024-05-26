632fc3d28cc8016a5fdedfce87b0981743ba2b06
Rizki Nuzul Muzaki <rizki@qiscus.net>
2024-05-26 08:27:35 +0700

    fix: remove console.info

--- src/main.js
+++ src/main.js
@@ -2,6 +2,5 @@ import { web } from "./application/web.js";
 import { logger } from "./application/logging.js";
 
 web.listen(3000, () => {
-    console.info("App start")
     logger.info("App start");
 });
